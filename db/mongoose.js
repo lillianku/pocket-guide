5384d154870d024063b3213ef7939285edaef1ff
Lilli Ku <lilli.c.ku@disney.com>
2018-05-07 13:45:44 -0700

    update datebase name

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Todos');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/pocketguide');
 
 module.exports = mongoose;
