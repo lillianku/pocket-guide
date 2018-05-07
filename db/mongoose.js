d49e4cba84e934027782613bd102fc7f08c98fb0
Lilli Ku <lilli.c.ku@disney.com>
2018-05-07 09:46:22 -0700

    require mongoose, database

---
+++ db/mongoose.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Todos');
+
+module.exports = mongoose;
