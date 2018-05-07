128e859bbc21bff4dcd7a0ea6e787584b6e56763
Lilli Ku <lilli.c.ku@disney.com>
2018-05-07 11:19:10 -0700

    get/post new places

--- routes/places.js
+++ routes/places.js
@@ -5,8 +5,31 @@ const
   Place = require('./../models/models.js')
 
 PlacesRouter.get("/", (req, res)=>{
-  res.render('places.ejs');
+  Place.find().then(places=>{
+      res.render('places.ejs', {
+        templatePlaces: places
+      });
+      // res.send(places);//Postman testing
+    }, error=>{
+      res.status(400).send('400 Bad Request');
+    });
 });
+
+PlacesRouter.post('/', (req, res) => {
+    let newPlace = new Place({
+      country: req.body.country,
+      city: req.body.city,
+      url: req.body.url,
+      notes: req.body.notes
+    });
+    newPlace.save().then(place => {
+      res.redirect('/places');
+    }, error => {
+      res.status(400).send('400 BAD REQUEST');
+    });
+});
+
+
 
 
 module.exports = PlacesRouter;
