3c89c5acb7eacce6b7cc610eac7ea2d84713a1a9
Lilli Ku <lilli.c.ku@disney.com>
2018-05-07 10:30:33 -0700

    create models for lists

---
+++ models/models.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const Place = mongoose.model('Place', {
+  country: {type: String},
+  city:{type: String},
+  url: {type: String},
+  notes: {type: String}
+});
+
+const Item = mongoose.model('Item', {
+  item: {type: String},
+  details: {type: String}
+});
+
+module.exports = Place, Item;
