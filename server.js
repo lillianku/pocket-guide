2020cd055f2a3dcfc07c296dfa66f78f8eee4ae5
Lilli Ku <lilli.c.ku@disney.com>
2018-05-07 10:29:55 -0700

    render ejs home page

---
+++ server.js
@@ -0,0 +1,32 @@
+const
+  express = require('express'),
+  app = express(),
+  port = process.env.PORT || 3000,
+  mongoose = require('./db/mongoose.js'),
+  bodyParser = require('body-parser'),
+  methodOverride = require('method-override'),
+  Place = require('./models/models.js'),
+  Item = require('./models/models.js')
+
+
+//MIDDLEWARE
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride((req, res) => {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method
+    delete req.body._method
+    return method
+  };
+}));
+
+app.set('view engine', 'ejs');
+app.use(express.static('static'));
+
+app.get('/', (req, res)=>{
+  res.render('home.ejs');
+});
+
+app.listen(port, ()=>{
+  console.log('Server is working');
+});
