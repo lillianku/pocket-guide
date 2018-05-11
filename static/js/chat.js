2e2229dd787859ec0eb739a0390c168d434f8b3b
Lilli Ku <lilli.c.ku@disney.com>
2018-05-10 17:18:29 -0700

    saves messages and reloads saved messages on each page load

--- static/js/chat.js
+++ static/js/chat.js
@@ -1,5 +1,19 @@
 $(function () {
 
+  //loaded stored messages
+  const loadMessages = function (message) {
+    const text = `${message.name}: ${message.message}`;
+    const $li = $('<li>', {text:text});
+    $('#messages').append($li);
+  };
+
+  $.get('/messages', messages=>{
+    const message = messages.results;
+    messages.forEach(loadMessages);
+    // $('#oldmessage').attr('style');
+    // setTimeout(function(){ $('#oldmessage').attr('style'); }, 1000);
+  });
+
   var socket = io();
   $('#form').submit(function(){
     const data = {message: $('#message').val(), name: $('#name').val()};
@@ -12,8 +26,8 @@ $(function () {
   socket.on('chat message', function(data){
     const text = `${data.name}: ${data.message}`;
     //https://stackoverflow.com/questions/45105992/node-js-send-data-to-backend-with-ajax
-    // $.post('/messages', { name: `${data.name}`, message:`${data.message}`});
-    $('#messages').prepend($('<li>').text(text));
+    $.post('/messages', { name: `${data.name}`, message:`${data.message}`});
+    $('#messages').append($('<li>').text(text));
   });
 
 });
